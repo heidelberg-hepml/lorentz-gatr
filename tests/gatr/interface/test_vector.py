afbe6762802285b122a65435f1fc43a0a9870a65
Jonas Spinner <jonasspinner@gmx.de>
2023-12-05 19:46:01 +0100

    Adapted interface: Remove PGA-specific objects; add Lorentz vector; keep scalar and pseudoscalar

---
+++ tests/gatr/interface/test_vector.py
@@ -0,0 +1,17 @@
+# Copyright (c) 2023 Qualcomm Technologies, Inc.
+# All rights reserved.
+import numpy as np
+import pytest
+import torch
+
+from gatr.interface import embed_vector, extract_vector
+from tests.helpers import BATCH_DIMS, TOLERANCES
+
+
+@pytest.mark.parametrize("batch_dims", BATCH_DIMS)
+def test_vector_embedding_consistency(batch_dims):
+    """Tests whether Lorentz vector embeddings into multivectors are cycle consistent."""
+    vectors = torch.randn(*batch_dims, 4)
+    multivectors = embed_vector(vectors)
+    vectors_reencoded = extract_vector(multivectors)
+    torch.testing.assert_close(vectors, vectors_reencoded, **TOLERANCES)
