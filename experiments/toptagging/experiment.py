45c734c5b77155a813738299c08f75958631e3d7
Jonas Spinner <jonasspinner@gmx.de>
2024-02-01 15:24:28 +0100

    Added toptagging

---
+++ experiments/toptagging/experiment.py
@@ -0,0 +1,218 @@
+import numpy as np
+import torch
+from torch_geometric.loader import DataLoader
+
+import os, time
+from omegaconf import OmegaConf, open_dict
+
+from sklearn.metrics import roc_curve, roc_auc_score, accuracy_score
+
+from experiments.base_experiment import BaseExperiment
+from experiments.toptagging.dataset import TopTaggingDataset
+from experiments.toptagging.plots import plot_mixer
+from experiments.logger import LOGGER
+from experiments.mlflow import log_mlflow
+
+import matplotlib.pyplot as plt
+
+MODEL_TITLE_DICT = {"GATr": "GATr", "Transformer": "Tr", "MLP": "MLP"}
+
+
+class TopTaggingExperiment(BaseExperiment):
+    def init_physics(self):
+        if (
+            self.cfg.model._target_
+            == "experiments.toptagging.wrappers.TopTaggingTransformerWrapper"
+        ):
+            assert (
+                not self.cfg.data.add_pairs
+            ), "data.add_pairs are not implemented for the default Transformer"
+            # assert not self.cfg.model.beam_reference, "model.beam_reference are not implemented for the default Transformer"
+
+        if not self.cfg.training.force_xformers:
+            LOGGER.warning(
+                f"Using training.force_xformers=False, this will slow down the network by a factor of 5-10."
+            )
+
+        with open_dict(self.cfg):
+            # extra mv channels for GATr
+            if (
+                self.cfg.model._target_
+                == "experiments.toptagging.wrappers.TopTaggingGATrWrapper"
+            ):
+                if self.cfg.model.beam_reference is not None:
+                    self.cfg.model.net.in_mv_channels += 1
+                if self.cfg.data.add_pairs:
+                    self.cfg.model.net.in_mv_channels += 2
+
+    def init_data(self):
+        data_path = os.path.join(
+            self.cfg.data.data_dir, f"toptagging_{self.cfg.data.dataset}.npz"
+        )
+        LOGGER.info(f"Loading top-tagging dataset from {data_path}")
+        t0 = time.time()
+        self.data_train = TopTaggingDataset(
+            data_path,
+            "train",
+            data_scale=None,
+            add_pairs=self.cfg.data.add_pairs,
+            dtype=self.dtype,
+        )
+        self.data_test = TopTaggingDataset(
+            data_path,
+            "test",
+            data_scale=self.data_train.data_scale,
+            add_pairs=self.cfg.data.add_pairs,
+            dtype=self.dtype,
+        )
+        self.data_val = TopTaggingDataset(
+            data_path,
+            "val",
+            data_scale=self.data_train.data_scale,
+            add_pairs=self.cfg.data.add_pairs,
+            dtype=self.dtype,
+        )
+        dt = time.time() - t0
+        LOGGER.info(f"Finished creating datasets after {dt:.2f} s = {dt/60:.2f} min")
+
+    def _init_dataloader(self):
+        self.train_loader = DataLoader(
+            dataset=self.data_train,
+            batch_size=self.cfg.training.batchsize,
+            shuffle=True,
+        )
+        self.test_loader = DataLoader(
+            dataset=self.data_test,
+            batch_size=self.cfg.training.batchsize,
+            shuffle=False,
+        )
+        self.val_loader = DataLoader(
+            dataset=self.data_val, batch_size=self.cfg.training.batchsize, shuffle=False
+        )
+
+        LOGGER.info(
+            f"Constructed dataloaders with "
+            f"train_batches={len(self.train_loader)}, test_batches={len(self.test_loader)}, val_batches={len(self.val_loader)}, "
+            f"batch_size={self.cfg.training.batchsize} (training), {self.cfg.evaluation.batchsize} (evaluation)"
+        )
+
+    def evaluate(self):
+        self.results = {}
+        # self.results["train"] = self._evaluate_single(self.train_loader, "train")
+        self.results["val"] = self._evaluate_single(self.val_loader, "val")
+        self.results["test"] = self._evaluate_single(self.test_loader, "test")
+
+    def _evaluate_single(self, loader, title):
+        LOGGER.info(
+            f"### Starting to evaluate model on {title} dataset with "
+            f"{len(loader.dataset.data_list)} elements, batchsize {loader.batch_size} ###"
+        )
+
+        # predictions
+        labels_true, labels_predict = [], []
+        self.model.eval()
+        with torch.no_grad():
+            for batch in loader:
+                batch = batch.to(self.device)
+                y_pred = self.model(batch)
+                y_pred = torch.nn.functional.sigmoid(y_pred)
+                labels_true.append(batch.label.cpu().float())
+                labels_predict.append(y_pred.cpu().float())
+        labels_true, labels_predict = torch.cat(labels_true), torch.cat(labels_predict)
+        assert labels_true.shape == labels_predict.shape
+
+        # bce loss
+        bce = torch.nn.functional.binary_cross_entropy_with_logits(
+            labels_predict, labels_true
+        )
+        if self.cfg.use_mlflow:
+            log_mlflow(f"eval.{title}.bce_loss", bce)
+        labels_true, labels_predict = labels_true.numpy(), labels_predict.numpy()
+
+        # accuracy
+        accuracy = accuracy_score(labels_true, np.round(labels_predict))
+        LOGGER.info(f"Accuracy on {title} dataset: {accuracy:.4f}")
+        if self.cfg.use_mlflow:
+            log_mlflow(f"eval.{title}.accuracy", accuracy)
+
+        # roc (fpr = epsB, tpr = epsS)
+        fpr, tpr, th = roc_curve(labels_true, labels_predict)
+        auc = roc_auc_score(labels_true, labels_predict)
+        LOGGER.info(f"AUC score on {title} dataset: {auc:.4f}")
+        if self.cfg.use_mlflow:
+            log_mlflow(f"eval.{title}.auc", auc)
+
+        # 1/epsB at fixed epsS
+        def get_rej(epsS):
+            idx = np.argmin(np.abs(tpr - epsS))
+            return 1 / fpr[idx]
+
+        rej03 = get_rej(0.3)
+        rej05 = get_rej(0.5)
+        LOGGER.info(
+            f"Rejection rate {title} dataset: {rej03:.0f} (epsS=0.3), {rej05:.0f} (epsS=0.5)"
+        )
+        if self.cfg.use_mlflow:
+            log_mlflow(f"eval.{title}.rej03", rej03)
+            log_mlflow(f"eval.{title}.rej05", rej05)
+
+        results = {
+            "labels_true": labels_true,
+            "labels_predict": labels_predict,
+            "fpr": fpr,
+            "tpr": tpr,
+            "auc": auc,
+            "bce": bce,
+        }
+        return results
+
+    def plot(self):
+        plot_path = os.path.join(self.cfg.run_dir, f"plots_{self.cfg.run_idx}")
+        os.makedirs(plot_path)
+        model_title = MODEL_TITLE_DICT[type(self.model.net).__name__]
+        title = model_title
+        LOGGER.info(f"Creating plots in {plot_path}")
+
+        if self.cfg.evaluate:
+            file = f"{plot_path}/roc.txt"
+            roc = np.stack(
+                (self.results["test"]["fpr"], self.results["test"]["tpr"]), axis=-1
+            )
+            np.savetxt(file, roc)
+
+        plot_dict = {}
+        if self.cfg.evaluate:
+            plot_dict = {"results_test": self.results["test"]}
+        if self.cfg.train:
+            plot_dict["train_loss"] = self.train_loss
+            plot_dict["val_loss"] = self.val_loss
+            plot_dict["train_lr"] = self.train_lr
+            plot_dict["train_metrics"] = self.train_metrics
+            plot_dict["val_metrics"] = self.val_metrics
+        plot_mixer(self.cfg, plot_path, title, plot_dict)
+
+    def _init_loss(self):
+        self.loss = torch.nn.BCEWithLogitsLoss()
+
+    def _batch_loss(self, batch):
+        batch = batch.to(self.device)
+        y_pred = self.model(batch)
+        loss = self.loss(y_pred, batch.label.to(self.dtype))
+        assert torch.isfinite(loss).all()
+
+        # compute cheap metrics
+        try:
+            labels_true, labels_predict = (
+                batch.label.float().cpu(),
+                torch.nn.functional.sigmoid(y_pred).detach().cpu(),
+            )
+            auc = roc_auc_score(labels_true, labels_predict)
+            accuracy = accuracy_score(labels_true, np.round(labels_predict))
+            metrics = {"auc": auc, "accuracy": accuracy}
+        except ValueError:
+            # only one jet class present because batchsize too small -> Can not compute metrics
+            metrics = {}
+        return loss, metrics
+
+    def _init_metrics(self):
+        return {"auc": [], "accuracy": []}
---
+++ experiments/toptagging/wrappers.py
@@ -0,0 +1,120 @@
+# Copyright (c) 2023 Qualcomm Technologies, Inc.
+# All rights reserved.
+# import dgl
+import numpy as np
+import torch
+from torch import nn
+
+from gatr.interface import embed_vector, extract_scalar
+from xformers.ops.fmha import BlockDiagonalMask
+
+from experiments.logger import LOGGER
+
+
+def xformers_mask(batch):
+    # efficient implementation, should use this for serious business
+    bincounts = torch.bincount(batch.batch).tolist()
+    return BlockDiagonalMask.from_seqlens(bincounts)
+
+
+def torch_mask(batch):
+    # inefficient implementation due to quadratic mask for very simple task
+    # could be implemented more efficiently by masking queries, keys and values before
+    # combining them in self-attention, this requires only a linear mask
+
+    # implemented only to allow debugging on cpu's
+    bincounts = torch.bincount(batch.batch).tolist()
+    blocks = [
+        torch.ones(i, i, dtype=torch.bool, device=batch.x.device) for i in bincounts
+    ]
+    return torch.block_diag(*blocks)
+
+
+class TopTaggingTransformerWrapper(nn.Module):
+    """
+    Baseline Transformer for top-tagging
+    This is mainly for debugging. I will not all features that we try for GATr
+    """
+
+    def __init__(self, net, force_xformers=True):
+        super().__init__()
+        self.net = net
+        self.force_xformers = force_xformers
+
+    def forward(self, batch):
+        if self.force_xformers:
+            mask = xformers_mask(batch)
+        else:
+            mask = torch_mask(batch)
+
+        # unsqueeze and squeeze to formally add the batch index (any better way of doing this?)
+        inputs = batch.x.unsqueeze(0)
+        outputs = self.net(inputs, attention_mask=mask)
+        logits = outputs.squeeze(0)[batch.is_global]
+
+        return logits
+
+
+class TopTaggingGATrWrapper(nn.Module):
+    def __init__(self, net, beam_reference, force_xformers=True):
+        super().__init__()
+        self.net = net
+        self.beam_reference = beam_reference
+        self.force_xformers = force_xformers
+
+    def forward(self, batch):
+        if self.force_xformers:
+            mask = xformers_mask(batch)
+        else:
+            mask = torch_mask(batch)
+
+        multivector, scalars = self.embed_into_ga(batch)
+        multivector_outputs, scalar_outputs = self.net(
+            multivector, scalars=scalars, attention_mask=mask
+        )
+        logits = self.extract_from_ga(batch, multivector_outputs, scalar_outputs)
+
+        return logits
+
+    def embed_into_ga(self, batch):
+        # unsqueeze channel dimension (already done in case data.add_pairs=true)
+        if len(batch.x.shape) == 2:
+            batch.x = batch.x.unsqueeze(1)
+
+        # add beam_reference
+        if self.beam_reference in ["photon", "spacelike"]:
+            # add another 4-momentum
+            beam = [1, 0, 0, 1] if self.beam_reference == "photon" else [0, 0, 0, 1]
+            beam = torch.tensor(beam, device=batch.x.device, dtype=batch.x.dtype)
+            beam = beam.unsqueeze(0).expand(batch.x.shape[0], 4).unsqueeze(1)
+
+            batch.x = torch.concatenate((batch.x, beam), dim=1)
+            multivector = embed_vector(batch.x)
+        elif self.beam_reference == "xyplane":
+            multivector = embed_vector(batch.x)
+
+            # add the x-y-plane, embedded as a bivector
+            # convention for bivector components: [tx, ty, tz, xy, xz, yz]
+            plane = torch.zeros(
+                multivector.shape[0], 1, 16, device=batch.x.device, dtype=batch.x.dtype
+            )
+            plane[..., 8] = 1
+            multivector = torch.concatenate((multivector, plane), dim=-2)
+        else:
+            multivector = embed_vector(batch.x)
+
+        scalars = torch.zeros(
+            multivector.shape[0], 1, device=batch.x.device, dtype=batch.x.dtype
+        )
+
+        # add batch index (needed for xformers attention)
+        multivector, scalars = multivector.unsqueeze(0), scalars.unsqueeze(0)
+        return multivector, scalars
+
+    def extract_from_ga(self, batch, multivector, scalars):
+
+        # remove batch index (0) and channel index (-1)
+        outputs = extract_scalar(multivector)
+        logits = outputs.squeeze([0, -1])[batch.is_global]
+
+        return logits
