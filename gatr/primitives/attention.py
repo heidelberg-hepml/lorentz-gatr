8358d5f4cc722806256acf88060a6ba024978073
Jonas Spinner <jonasspinner@gmx.de>
2024-02-02 18:55:03 +0100

    Extra expand() needed to make multiquery work with xformers (not sure why the original gatr code did not need it; might be because I am using a newer xformers version)

--- gatr/primitives/attention.py
+++ gatr/primitives/attention.py
@@ -121,6 +121,9 @@ def scaled_dot_product_attention(
         of shape [batch, head, item, d]
     """
     if FORCE_XFORMERS or isinstance(attn_mask, AttentionBias):
+        if key.shape[1] != query.shape[1]: # required to make multi_query work
+            key = key.expand(key.shape[0], query.shape[1], *key.shape[2:])
+            value = value.expand(value.shape[0], query.shape[1], *value.shape[2:])
         query = query.transpose(
             1, 2
         )  # [batch, head, item, d] -> [batch, item, head, d]
