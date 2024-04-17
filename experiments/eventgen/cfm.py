Fixed log_prob calculation issues for non-trivial spaces (and related shape issues)

experiments/eventgen/cfm.py
@@ -13,7 +13,11 @@ from experiments.eventgen.distributions import (
     FittedLogPtPhiEtaLogM2,
 )
 import experiments.eventgen.coordinates as c
-from experiments.eventgen.coordinates import convert_coordinates, convert_velocity
+from experiments.eventgen.coordinates import (
+    convert_coordinates,
+    convert_velocity,
+    convert_log_prob,
+)
 
 
 class GaussianFourierProjection(nn.Module):
@@ -315,17 +319,23 @@ class CFM(nn.Module):
                     xt_sampling, self.coordinates_sampling, self.coordinates_network
                 )
                 vt_network = self.get_velocity(xt_network, t, ijet=ijet)
+                dlogp_dt_network = (
+                    self.trace_fn(vt_network, xt_network).unsqueeze(-1).detach()
+                )
+                xt_network, vt_network = xt_network.detach(), vt_network.detach()
                 vt_sampling = convert_velocity(
                     vt_network,
                     xt_network,
                     self.coordinates_network,
                     self.coordinates_sampling,
                 )[0]
-                xt_sampling = convert_coordinates(
-                    xt_network, self.coordinates_network, self.coordinates_sampling
-                )  # required to not destroy graph
-                dlogp_dt = self.trace_fn(vt_sampling, xt_sampling).unsqueeze(-1)
-            return vt_sampling.detach(), dlogp_dt.detach()
+                dlogp_dt_sampling = convert_log_prob(
+                    dlogp_dt_network,
+                    xt_network,
+                    self.coordinates_network,
+                    self.coordinates_sampling,
+                )[0]
+            return vt_sampling, dlogp_dt_sampling
 
         # solve ODE in sampling space
         x0_sampling = self.coordinates_sampling.fourmomenta_to_x(x0_fourmomenta)

experiments/eventgen/transforms.py
@@ -43,13 +43,13 @@ class BaseTransform(nn.Module):
         return v_x
 
     def logdetjac_forward(self, x, y):
-        logdetjac = self._detjac_forward(x, y).abs().log().sum(dim=-1)
+        logdetjac = self._detjac_forward(x, y).abs().log().sum(dim=-1, keepdims=True)
         assert torch.isfinite(logdetjac).all()
         return logdetjac
 
     def logdetjac_inverse(self, y, x):
         # log(det(J^-1)) = log(1/det(J)) = -log(det(J))
-        logdetjac = -self._detjac_forward(x, y).abs().log().sum(dim=-1)
+        logdetjac = -self._detjac_forward(x, y).abs().log().sum(dim=-1, keepdims=True)
         assert torch.isfinite(logdetjac).all()
         return logdetjac
 

tests/experiments/eventgen/test_transforms.py
@@ -363,8 +363,12 @@ def test_logdetjac(transforms, distribution, experiment_np, nevents):
         jac_inv_autograd.append(inv_autograd)
     jac_fw_autograd = torch.stack(jac_fw_autograd, dim=-2)
     jac_inv_autograd = torch.stack(jac_inv_autograd, dim=-2)
-    logdetjac_fw_autograd = torch.linalg.det(jac_fw_autograd).abs().log().sum(dim=-1)
-    logdetjac_inv_autograd = torch.linalg.det(jac_inv_autograd).abs().log().sum(dim=-1)
+    logdetjac_fw_autograd = (
+        torch.linalg.det(jac_fw_autograd).abs().log().sum(dim=-1, keepdims=True)
+    )
+    logdetjac_inv_autograd = (
+        torch.linalg.det(jac_inv_autograd).abs().log().sum(dim=-1, keepdims=True)
+    )
 
     # compare logdetjac to autograd
     torch.testing.assert_close(logdetjac_fw, logdetjac_fw_autograd, **TOLERANCES)