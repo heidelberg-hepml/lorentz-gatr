763a5a1b61f04836d5315f3757fcfac8d351d4b0
Jonas Spinner <jonasspinner@gmx.de>
2024-07-16 18:04:52 +0200

    Remove duplicated optimizer options

--- experiments/base_experiment.py
+++ experiments/base_experiment.py
@@ -363,21 +363,6 @@ class BaseExperiment:
                 eps=self.cfg.training.eps,
                 weight_decay=self.cfg.training.weight_decay,
             )
-        elif self.cfg.training.optimizer == "RAdam":
-            self.optimizer = torch.optim.RAdam(
-                self.model.parameters(),
-                lr=self.cfg.training.lr,
-                betas=self.cfg.training.betas,
-                eps=self.cfg.training.eps,
-            )
-        elif self.cfg.training.optimizer == "AdamW":
-            self.optimizer = torch.optim.AdamW(
-                self.model.parameters(),
-                lr=self.cfg.training.lr,
-                betas=self.cfg.training.betas,
-                eps=self.cfg.training.eps,
-                weight_decay=self.cfg.training.weight_decay,
-            )
         elif self.cfg.training.optimizer == "Lion":
             self.optimizer = Lion(
                 self.model.parameters(),
