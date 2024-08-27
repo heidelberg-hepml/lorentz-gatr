ec794761f455a3a5ac51b7d29e892667961a7b35
Jonas Spinner <jonasspinner@gmx.de>
2024-08-27 20:55:03 +0200

    Undo changes for onshell generation with CFM (not fully tested, part of another pull request)

--- experiments/eventgen/cfm.py
+++ experiments/eventgen/cfm.py
@@ -135,10 +135,6 @@ class CFM(nn.Module):
         )
         return vp_sampling, xt_sampling
 
-    def handle_velocity(self, v):
-        # default: do nothing
-        return v
-
     def batch_loss(self, x0_fourmomenta, ijet):
         """
         Construct the conditional flow matching objective
@@ -233,7 +229,6 @@ class CFM(nn.Module):
             vt_sampling, xt_sampling = self.get_velocity_sampling(
                 xt_network, t, ijet=ijet
             )
-            vt_sampling = self.handle_velocity(vt_sampling)
 
             # collect trajectories
             if save_trajectory:
@@ -349,7 +344,6 @@ class CFM(nn.Module):
                     xt_sampling, self.coordinates_sampling, self.coordinates_network
                 )
                 vt_sampling = self.get_velocity_sampling(xt_network, t, ijet=ijet)[0]
-                vt_sampling = self.handle_velocity(vt_sampling)
                 dlogp_dt_sampling = (
                     -self.trace_fn(vt_sampling, xt_sampling).unsqueeze(-1).detach()
                 )
@@ -542,19 +536,14 @@ class EventCFM(CFM):
 
     def sample(self, *args, **kwargs):
         fourmomenta = super().sample(*args, **kwargs)
-        return fourmomenta
 
-    def handle_velocity(self, v):
-        if self.cfm.coordinates_straight in [
-            "PPPM2",
-            "PPPLogM2",
-            "StandardPPPLogM2",
-            "PtPhiEtaM2",
-            "LogPtPhiEtaM2",
-            "PtPhiEtaLogM2",
-            "LogPtPhiEtaLogM2",
-            "StandardLogPtPhiEtaLogM2",
-        ]:
-            # manually set mass velocity of onshell events to zero
-            v[..., self.onshell_list, 3] = 0.0
-        return v
+        # enforce onshell-ness
+        mass = (
+            torch.tensor(self.onshell_mass)
+            .unsqueeze(0)
+            .to(fourmomenta.device, dtype=fourmomenta.dtype)
+        ) / self.units
+        fourmomenta[..., self.onshell_list, 0] = torch.sqrt(
+            mass**2 + torch.sum(fourmomenta[..., self.onshell_list, 1:] ** 2, dim=-1)
+        )
+        return fourmomenta
