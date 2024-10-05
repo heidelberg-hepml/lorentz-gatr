23375226907f6252faf3c46011863daaba1c7691
Jonas Spinner <jonasspinner@gmx.de>
2024-10-05 17:52:02 +0200

    Draft for dnet transformer

--- experiments/eventgen/dnet.py
+++ experiments/eventgen/dnet.py
@@ -1,6 +1,7 @@
 import torch
 from torch import nn
 from experiments.baselines.mlp import MLP
+from experiments.baselines.transformer import Transformer
 from experiments.eventgen.cfm import GaussianFourierProjection
 
 
@@ -53,4 +54,41 @@ class DisplacementMLP(DisplacementNet):
             dim=-1,
         )
         phi = self.net(embedding).reshape_as(x_base)
+        return phi
+
+
+class DisplacementTransformer(DisplacementNet):
+    def __init__(
+        self,
+        hidden_channels,
+        num_blocks,
+        num_heads,
+        n_features,
+        embed_t_dim,
+        embed_t_scale,
+    ):
+        super().__init__(embed_t_dim, embed_t_scale)
+        self.net = Transformer(
+            in_channels=2 + embed_t_dim + n_features,
+            out_channels=1,
+            hidden_channels=hidden_channels,
+            num_blocks=num_blocks,
+            num_heads=num_heads,
+        )
+
+    def forward(self, x_base, x_target, t):
+        x_base_emb, x_target_emb, t_emb = self.embed_everything(x_base, x_target, t)
+        x_base_emb, x_target_emb = x_base_emb.unsqueeze(-1), x_target_emb.unsqueeze(-1)
+        t_emb = torch.repeat_interleave(
+            t_emb.unsqueeze(-2), x_base_emb.shape[-2], dim=-2
+        )
+        idx = torch.arange(x_base_emb.shape[-2], device=x_base_emb.device)
+        idx_emb = torch.nn.functional.one_hot(idx).repeat(
+            [1 for _ in range(len(x_base_emb.shape))]
+        )
+        idx_emb = idx_emb.repeat(list(x_base_emb.shape[:-2]) + [1, 1])
+        embedding = torch.cat((x_base_emb, x_target_emb, t_emb, idx_emb), dim=-1)
+
+        phi = self.net(embedding)
+        phi = phi.reshape_as(x_base)
         return phi
