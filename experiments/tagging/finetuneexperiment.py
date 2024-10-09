5ca8303e83892f85dec77efaeffcfc9bcca5633e
Jonas Spinner <jonasspinner@gmx.de>
2024-10-09 15:26:50 +0200

    Properly 'data.add_energy' in finetuning

--- experiments/tagging/finetuneexperiment.py
+++ experiments/tagging/finetuneexperiment.py
@@ -39,6 +39,7 @@ class TopTaggingFineTuneExperiment(TopTaggingExperiment):
             self.cfg.data.beam_token = warmstart_cfg.data.beam_token
             self.cfg.data.add_time_reference = warmstart_cfg.data.add_time_reference
             self.cfg.data.add_pt = warmstart_cfg.data.add_pt
+            self.cfg.data.add_energy = warmstart_cfg.data.add_energy
             self.cfg.data.reinsert_channels = warmstart_cfg.data.reinsert_channels
             self.cfg.data.rescale_data = warmstart_cfg.data.rescale_data
 
