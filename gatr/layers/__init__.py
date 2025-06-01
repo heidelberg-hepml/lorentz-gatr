0f1d9a2b6960c1a7c7c52deb832403eba57bae69
Jonas Spinner <jonasspinner@gmx.de>
2025-06-01 13:53:57 +0200

    Remove cross-attention from code

--- gatr/layers/__init__.py
+++ gatr/layers/__init__.py
@@ -1,7 +1,6 @@
-from .attention.config import SelfAttentionConfig, CrossAttentionConfig
+from .attention.config import SelfAttentionConfig
 from .attention.positional_encoding import ApplyRotaryPositionalEncoding
 from .attention.self_attention import SelfAttention
-from .attention.cross_attention import CrossAttention
 from .dropout import GradeDropout
 from .layer_norm import EquiLayerNorm
 from .linear import EquiLinear
@@ -10,4 +9,3 @@ from .mlp.mlp import GeoMLP
 from .mlp.config import MLPConfig
 from .mlp.nonlinearities import ScalarGatedNonlinearity
 from .gatr_block import GATrBlock
-from .conditional_gatr_block import ConditionalGATrBlock
